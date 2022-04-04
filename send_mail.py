cba7eb38ffb94b54f77c381988ea41b4adba1d14
Osmar <osmar.gascho@nbwdigital.com.br>
2022-04-04 13:22:13 -0300

    fix

--- send_mail.py
+++ send_mail.py
@@ -21,7 +21,7 @@ def send_mail(data):
     server_smtp.login(authdata["USER"],authdata["PASSWORD"])
     msg["Subject"] = "Contato Site"
     msg["From"] = "noreply@lunelli.com.br"
-    msg["To"] = "suporte@nossaloja.zendesk.com"
+    msg["To"] = "sac@grupolunelli.com"
     message=""
     message+="Dúvida sobre Lojas Virtuais no site lunelli.com.br:\n\n"
     message+="Nome: "+data["00N4W00000S0yWc"]+"\n"
